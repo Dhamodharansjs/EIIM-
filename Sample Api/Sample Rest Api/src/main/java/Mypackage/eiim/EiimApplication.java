6af1f33a03047aa29d6c705f320ec0e8cdc1719d
Dhamodharan Sundar <157094609+Dhamodharansjs@users.noreply.github.com>
2024-02-24 00:22:23 +0530

    Sample Api

---
+++ Sample Api/Sample Rest Api/src/main/java/Mypackage/eiim/EiimApplication.java
@@ -0,0 +1,13 @@
+package Mypackage.eiim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EiimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EiimApplication.class, args);
+	}
+
+}
---
+++ Sample Api/Sample Rest Api/src/test/java/Mypackage/eiim/EiimApplicationTests.java
@@ -0,0 +1,13 @@
+package Mypackage.eiim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EiimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
